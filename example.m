bb65c98d7d4e5e10cb150f8fd984ec1fd039080a
fbreve <fbreve@gmail.com>
2019-01-23 12:24:01 -0200

    Update example.m

--- example.m
+++ example.m
@@ -5,7 +5,7 @@
 % Propagation through Complex Networks. Expert System With Applications, 
 % v. 123, p.18 – 33, 2019.
 %
-% by Fabricio Breve - 21/01/2019
+% by Fabricio Breve - 23/01/2019
 %
 % Loading example image
 img = imread('ralph.jpg');
@@ -29,7 +29,11 @@ imgres3 = repmat(imgres,1,1,3)./255;
 imgseg = img .* imgres3;
 imgseg(imgres3==0)=255;
 % Showing segmentation results
-subplot(2,2,1), imshow(img),
-subplot(2,2,2), imshow(imgslab),
-subplot(2,2,3), imshow(imgres),
-subplot(2,2,4), imshow(imgseg);
+figure('units','normalized','outerposition',[0 0 1 1])
+imgslabmix = imread('ralph-scribblemix.jpg');
+subplot(2,3,1), imshow(img), title('Original image'),
+subplot(2,3,2), imshow(imgslab), title('Scribbles (user-input)'),
+subplot(2,3,3), imshow(imgslabmix), title('Scribbles overlaying original image'),
+subplot(2,3,4), imshow(gt), title('Ground-truth'),
+subplot(2,3,5), imshow(imgres), title('Segmentation results (mask)'),
+subplot(2,3,6), imshow(imgseg); title('Segmentation results (extracted foreground)'),
